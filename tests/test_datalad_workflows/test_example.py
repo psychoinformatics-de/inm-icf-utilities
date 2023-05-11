6ded0d19ea8c8cc7aa45e2693e921a479097cc48
Christian Monch <christian.moench@web.de>
2023-05-11 13:09:14 +0200

    fix datalad_next requirements to use beta version

--- tests/test_datalad_workflows/test_example.py
+++ tests/test_datalad_workflows/test_example.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
+from datalad.api import download
 from datalad.distribution.dataset import Dataset
+from datalad_next.utils import chpwd
 
 
 protocol = 'http'
@@ -9,10 +11,15 @@ protocol = 'http'
 def test_example(tmp_path: Path):
     dataset_path = tmp_path / 'ds'
     dataset = Dataset(dataset_path).create()
-    dataset.download(
+
+    res1 = download(
         f'{protocol}://test.user:secret_1@localhost/'
-        f'~appveyor/study_1/visit_1_dicom.tar')
-    dataset.download(
-        f'{protocol}://localhost/~appveyor/study_1/visit_1_dicom.tar.md5sum')
+        f'~appveyor/study_1/visit_1_dicom.tar',
+        dataset=dataset)
+    res2 = download(
+        f'{protocol}://test.user:secret_1@localhost/'
+        f'~appveyor/study_1/visit_1_dicom.tar.md5sum',
+        dataset=dataset)
+
     elements = dataset_path.iterdir()
     print(elements)
