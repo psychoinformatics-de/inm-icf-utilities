124e040b6e86e064fa7c44ca30743563d69d0045
Christian Monch <christian.moench@web.de>
2023-05-13 04:05:49 +0200

    add dicom_server fixture to allow local tests
    This commit adds a fixture called `dicom_server`
    that provides server details to the tests. If
    the test is not executed in an appveyor VM,
    the fixture will create a local http-server,
    that serves from the directory specified by
    the environment variable `INM_ICF_TEST_STUDIES`.
    
    `pytest-env` has been added to the dependencies
    to make environment variables accessible within
    the tests

---
+++ tests/environment.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+import os
+from tempfile import TemporaryDirectory
+
+import pytest
+
+from datalad_next.tests.utils import HTTPPath
+
+
+local_eny_key = 'INM_ICF_TEST_STUDIES'
+
+
+@pytest.fixture(autouse=False, scope='session')
+def dicom_server():
+    if os.environ.get('APPVEYOR', None) == 'true':
+        yield dict(
+            base_url='http://localhost/~appveyor',
+            studies=os.environ['STUDIES'].split(),
+            user='test.user',
+            secret='secret_1')
+    else:
+        study_dir = os.environ.get(local_eny_key, None)
+        if not study_dir:
+            raise ValueError(
+                'Cannot execute tests locally, because the environment '
+                f'variable ``{local_eny_key}´´ is not defined. Set the '
+                f'environment variable ``{local_eny_key}´´ in a startup '
+                f'script, e.g. ~/.bashrc, to point to a local directory that '
+                f'contains study data, as defined in RFD0034.')
+        server = HTTPPath(
+            study_dir,
+            auth=('test.user.local', 'secret_1_local')
+        )
+        with server:
+            yield dict(
+                base_url=server.url,
+                studies=[],
+                user='test.user.local',
+                secret='secret_1_local')
--- tests/test_datalad_workflows/test_example.py
+++ tests/test_datalad_workflows/test_example.py
@@ -10,36 +10,39 @@ from datalad.api import (
 from datalad.support.exceptions import IncompleteResultsError
 from datalad.utils import chpwd
 
-
-protocol = 'http'
+# Fixture to get the configuration of the test environment
+from ..environment import dicom_server
 
 
 def _check_results(results: list[dict]):
     assert all(result['status'] == 'ok' for result in results)
 
 
-def test_example_unauthorized():
+def test_example_unauthorized(dicom_server):
+    print(dicom_server)
     with pytest.raises(IncompleteResultsError):
         download(
-            f'{protocol}://localhost/~appveyor/study_1/visit_1_dicom.tar')
+            f'{dicom_server["base_url"]}/study_1/visit_1_dicom.tar',
+            result_renderer='disabled')
 
 
-def test_example_authorized(tmp_path: Path):
+def test_example_authorized(dicom_server, tmp_path: Path):
+    print(dicom_server)
     results = credentials(
         'set',
         name='test_creds',
         spec={
             'type': 'user_password',
-            'user': 'test.user',
-            'secret': 'secret_1'})
+            'user': dicom_server['user'],
+            'secret': dicom_server['secret']})
     _check_results(results)
 
     with chpwd(tmp_path):
         results = download(
-            f'{protocol}://localhost/~appveyor/study_1/visit_1_dicom.tar',
-            credential='test_creds')
+            f'{dicom_server["base_url"]}/study_1/visit_1_dicom.tar',
+            credential='test_creds',
+            result_renderer='disabled')
         _check_results(results)
 
     elements = [Path(e).parts[-1] for e in tmp_path.iterdir()]
-    print(elements)
     assert 'visit_1_dicom.tar' in elements
