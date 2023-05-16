e0883a21faf30203fde2ad00eac935ba15c46dca
Michael Hanke <michael.hanke@gmail.com>
2023-05-16 09:08:06 +0200

    Reenable Git >= 2.32 fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,17 +3,13 @@ from datalad.conftest import setup_package
 # fixture setup
 from datalad_next.tests.fixtures import (
     # no test can leave global config modifications behind
-    # TODO: We cannot use this right now. It requires Git >= 2.32, but the
-    # deployment target only has 2.30
-    #check_gitconfig_global,
+    check_gitconfig_global,
     # no test can leave secrets behind
     check_plaintext_keyring,
     # function-scope credential manager
     credman,
     # function-scope config manager
-    # TODO: We cannot use this right now. It requires Git >= 2.32, but the
-    # deployment target only has 2.30
-    #datalad_cfg,
+    datalad_cfg,
     # function-scope temporary keyring
     tmp_keyring,
     # function-scope, Dataset instance
