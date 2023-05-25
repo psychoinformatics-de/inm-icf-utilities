d2e54ef82a1276cbb00910ac6fcd3c5be0c18d7b
Christian Monch <christian.moench@web.de>
2023-05-25 20:54:49 +0200

    add dataladification test
    This commit adds a dataladification test which
    uses an http-URL instead of https (since there
    is no https support in the server yet).
    
    Due to http usage, the final cloning step will
    fail when attempting to donwload an annex-location

--- tests/test_datalad_workflows/test_dataladification.py
+++ tests/test_datalad_workflows/test_dataladification.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 
 import os
 from pathlib import Path
+from typing import Iterable
 
-from datalad.api import create
+from datalad.api import clone
 from datalad.distribution.dataset import Dataset
 from datalad.runner.runner import WitlessRunner
 from datalad.tests.utils_pytest import on_appveyor
+from datalad_next.credman import CredentialManager
 
 
-existing_studies = os.environ['STUDIES'].split()
 existing_visits = ['visit_a', 'visit_b']
 
 
@@ -20,9 +21,9 @@ def run_script(name: str,
                ):
 
     script_path = Path(*(Path(__file__).parts[:-3] + ('bin',))) / name
-    runner = WitlessRunner(cwd=working_directory)
+    runner = WitlessRunner(cwd=working_directory, env=dict(os.environ))
     runner.run(
-        (['sudo'] if on_appveyor else []) + [
+        (['sudo', '-E', '--preserve-env=PATH'] if on_appveyor else []) + [
             str(script_path),
             '--id',
             study_id,
@@ -31,32 +32,93 @@ def run_script(name: str,
     )
 
 
-def dataladify_visits(studies: list[str], visits: list[str]):
+def dataladify_visits(studies_dir: Path,
+                      studies: list[str],
+                      visits: list[str]
+                      ):
     for study in studies:
         for visit in visits:
             run_script(
                 'dataladify_studyvisit',
-                '/data/archives',
+                studies_dir,
                 study, visit
             )
 
 
-def clone_visit(dataset: Dataset, study: str, visit: str):
-    dataset.clone(
+def get_clone_annex_url(source_url: str,
+                        study: str,
+                        visit: str
+                        ) -> tuple[str, str]:
+
+    if source_url.endswith('/'):
+        source_url = source_url[:-1]
+
+    return (
         'datalad-annex::?type=external&externaltype=uncurl'
-        f'&url=https://data.inm-icf.de/{study}/{visit}_{{annex_key}}'
-        '&encryption=none')
+        f'&url={source_url}/{study}/{visit}_{{{{annex_key}}}}'
+        '&encryption=none',
+        f'{source_url}/Protected',
+    )
 
 
-def test_dataladification(tmp_path: Path):
+def add_credentials(urls: Iterable[str],
+                    credentials: dict,
+                    credman: CredentialManager,
+                    ):
+
+    # Add credentials for all URLs to credential manager
+    for index, url in enumerate(urls):
+        kwargs = {
+            **credentials,
+            'name': f'icf-test-credentials-{index}',
+            'realm': url
+        }
+        result = credman.set(**kwargs)
+        assert result is not None
+
+
+def clone_visit(path: Path,
+                source_url: str,
+                study: str,
+                visit: str,
+                credentials: dict,
+                credman: CredentialManager,
+                ) -> Dataset:
+
+    # Get involved URLs and set credentials for `auth_url`
+    clone_url, auth_url = get_clone_annex_url(source_url, study, visit)
+    add_credentials([auth_url], credentials, credman)
+
+    # Clone a visit from the clone URL
+    return clone(source=clone_url, path=path)
+
+
+def test_dataladification(tmp_path: Path,
+                          test_studies_dir: str,
+                          test_study_names: list[str],
+                          data_webserver: str,
+                          dataaccess_credential: dict,
+                          credman: CredentialManager,
+                          ):
+
     # Perform dataladification
-    dataladify_visits(existing_studies, existing_visits)
+    dataladify_visits(
+        Path(test_studies_dir),
+        test_study_names,
+        existing_visits,
+    )
 
     # Try to clone the datasets and fetch the dicom tarfile
-    for study in existing_studies:
+    for study in test_study_names:
         for visit in existing_visits:
-            dataset = create(tmp_path / f'ds_{study}_{visit}')
-            clone_visit(dataset, study, visit)
-            # Try to get the tar file and the dicoms
+            dataset = clone_visit(
+                tmp_path / f'ds_{study}_{visit}',
+                data_webserver,
+                study,
+                visit,
+                dataaccess_credential,
+                credman,
+            )
+            # Try to get the tar file and the DICOMs
             dataset.get(f'icf/{visit}_dicom.tar')
             dataset.get(f'{study}_{visit}')
